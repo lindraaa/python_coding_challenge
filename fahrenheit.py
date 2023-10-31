49b69f81c980e2e1c9c563e0a82df39a64bce7ea
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-10-31 22:49:39 +0800

    Update fahrenheit.py

--- fahrenheit.py
+++ fahrenheit.py
@@ -11,6 +11,7 @@ def celsius_to_fahrenheit(celcius, conv):
         print("Error")
 num = float(input("Enter the celcius: "))
 
+
 ans = input("Enter the conversion(fahrenheit,kelvin) ")
 
 celsius_to_fahrenheit(num,ans)
