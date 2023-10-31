63fc0d370790235d95953690c774d088c94986a4
lindraaa <98926528+lindraaa@users.noreply.github.com>
2023-10-31 22:47:46 +0800

    Update area.py

--- area.py
+++ area.py
@@ -3,7 +3,5 @@ def calculate_circle_area(radius):
     total = 3.141559*(radius**2)
     return total
 
-
-
 num = float(input("Enter the radius of a circle: "))
 print("The area is --> ", round(calculate_circle_area(num)))
